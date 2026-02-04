2d72b93d11ededccf236d6c7e22a2ad8981269a4
hariharansara <144925342+hariharansara@users.noreply.github.com>
2026-02-04 12:05:14 +0530

    Add files via upload

---
+++ demo/demo/src/main/java/database.demo/entity/Student.java
@@ -0,0 +1,106 @@
+package database.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    private int student_id;
+    private String first_name;
+    private String last_name;
+    private String gender;
+    private LocalDate date_of_birth;
+    private String email;
+    private String phone;
+    private String department;
+    private int year_of_study;
+    private double gpa;
+
+    // ===== GETTERS & SETTERS =====
+
+    public int getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(int student_id) {
+        this.student_id = student_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(LocalDate date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getYear_of_study() {
+        return year_of_study;
+    }
+
+    public void setYear_of_study(int year_of_study) {
+        this.year_of_study = year_of_study;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
